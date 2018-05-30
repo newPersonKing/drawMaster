194489ecd892fd6069bcf688fe3a0a92f9ccc982
guoyong <guoyong@emcc.net.com>
2018-05-30 17:41:48 +0800

    diyici

---
+++ app/src/main/java/com/divyanshu/androiddraw/DrawAdapter.kt
@@ -0,0 +1,45 @@
+package com.divyanshu.androiddraw
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_view.view.*
+
+const val IMAGE_PATH = "image_path"
+/*构造函数一般都是val val 不能再次赋值 但是集合可以改变内容*/
+class DrawAdapter(private val context: Context, private val imageList: ArrayList<String>) : RecyclerView.Adapter<DrawAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val path = imageList[holder.adapterPosition]
+        Glide.with(context).load(path).into(holder.drawImage)
+        holder.drawImage.setOnClickListener {
+            val intent = Intent(context, ImageActivity::class.java)
+            intent.putExtra(IMAGE_PATH,path)
+            context.startActivity(intent)
+        }
+    }
+
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        val drawImage: ImageView = itemView.image_draw
+    }
+
+    fun addItem(uri: Uri){
+        imageList.add(uri.toString())
+        notifyItemInserted(imageList.size-1)
+    }
+}
---
+++ app/src/main/java/com/divyanshu/androiddraw/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.divyanshu.androiddraw
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import com.divyanshu.draw.activity.DrawingActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
+import kotlin.collections.ArrayList
+
+private const val REQUEST_CODE_DRAW = 101
+private const val PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 102
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: DrawAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE)
+        }else{
+            adapter = DrawAdapter(this,getFilesPath())
+            recycler_view.adapter = adapter
+        }
+        fab_add_draw.setOnClickListener {
+            val intent = Intent(this, DrawingActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_DRAW)
+        }
+    }
+
+    private fun getFilesPath(): ArrayList<String>{
+        val resultList = ArrayList<String>()
+
+        val imageDir = "${Environment.DIRECTORY_PICTURES}/Android Draw/"
+        val path = Environment.getExternalStoragePublicDirectory(imageDir)/*返回值是file*/
+        path.mkdirs()
+        val imageList = path.listFiles()
+        for (imagePath in imageList){
+            resultList.add(imagePath.absolutePath)
+        }
+        return resultList
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (data != null && resultCode == Activity.RESULT_OK) {
+            when(requestCode){
+                REQUEST_CODE_DRAW -> {
+                    val result= data.getByteArrayExtra("bitmap")
+                    val bitmap = BitmapFactory.decodeByteArray(result, 0, result.size)
+                    saveImage(bitmap)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                    adapter = DrawAdapter(this,getFilesPath())
+                    recycler_view.adapter = adapter
+                }else{
+                    finish()
+                }
+                return
+            }
+            else -> {}
+        }
+    }
+
+    private fun saveImage(bitmap: Bitmap) {
+        val imageDir = "${Environment.DIRECTORY_PICTURES}/Android Draw/"
+        val path = Environment.getExternalStoragePublicDirectory(imageDir)
+        val file = File(path, UUID.randomUUID().toString()+".png")
+        path.mkdirs()
+        file.createNewFile()
+        val outputStream = FileOutputStream(file)
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream)
+        outputStream.flush()
+        outputStream.close()
+        updateRecyclerView(Uri.fromFile(file))
+    }
+
+    private fun updateRecyclerView(uri: Uri) {
+        adapter.addItem(uri)
+    }
+}
